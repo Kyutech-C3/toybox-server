25dc395cc6fb92c65076bf6f173440f67893158d
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-09-26 00:59:51 +0900

    [add] #14 api/v1/tags/{id}の実装

--- cruds/tags/tag.py
+++ cruds/tags/tag.py
@@ -7,6 +7,20 @@ def create_tag(db: Session, name: str, color_code: str, community_id: str) -> Ta
     if name == '':
         raise HTTPException(status_code=400, detail="Name is empty")
 
+    result_by_name_and_community_id = db.query(models.Tag).filter(models.Tag.community_id == community_id, models.Tag.name == name).first()
+    if result_by_name_and_community_id != None:
+        raise HTTPException(
+            status_code=400,
+            detail="The tag is exist"
+        )
+    
+    result_by_community_id = db.query(models.Community).filter(models.Community.id == community_id).first()
+    if result_by_community_id is None:
+        raise HTTPException(
+            status_code=400,
+            detail="The community is not exist"
+        )
+
     tag_orm = models.Tag(
         name=name,
         community_id=community_id,
@@ -22,4 +36,14 @@ def create_tag(db: Session, name: str, color_code: str, community_id: str) -> Ta
 
     tag = Tag.from_orm(tag_orm)
 
+    return tag
+
+def get_tag(db: Session, tag_id: str) -> Tag:
+    result_by_id = db.query(models.Tag).filter(models.Tag.id == tag_id).first()
+    if result_by_id == None:
+        raise HTTPException(
+            status_code=400,
+            detail="The tag specified by id is not exist"
+        )
+    tag = Tag.from_orm(result_by_id)
     return tag
--- routers/tags/tag.py
+++ routers/tags/tag.py
@@ -5,11 +5,17 @@ from db import get_db
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from cruds.users.auth import GetCurrentUser
-from cruds.tags.tag import create_tag
+from cruds.tags.tag import create_tag, get_tag
 
 tag_router = APIRouter()
 
 @tag_router.post('', response_model=Tag)
 async def post_tag(payload: BaseTag, db: Session = Depends(get_db), user: User = Depends(GetCurrentUser())):
     tag = create_tag(db, payload.name, payload.color, payload.community_id)
+    return tag
+
+
+@tag_router.get('/{tag_id}', response_model=Tag)
+async def post_tag(tag_id: str, db: Session = Depends(get_db), user: User = Depends(GetCurrentUser())):
+    tag = get_tag(db, tag_id)
     return tag
