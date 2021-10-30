d17ba3fdd5693e938a80b1e21c989a7ab6fcd51c
PigeonsHouse <bullbull1230@gmail.com>
2021-10-31 00:45:39 +0900

    [update] 作品投稿とアセットの投稿の修正

--- cruds/works/__init__.py
+++ cruds/works/__init__.py
@@ -1,31 +1,62 @@
 from typing import List
 from fastapi import HTTPException
+from cruds.url_infos import create_url_info
 from db import models
 from sqlalchemy.orm.session import Session
+from schemas.url_info import BaseUrlInfo
 from schemas.work import Work
 import markdown
 
-def create_work(db: Session, title: str, description: str, work_url: str, github_url: str, user_id: str, community_id: str, private: bool) -> Work:
+def start_create_work(db: Session) -> str:
+    work_orm = models.Work(
+        title = '',
+        description = '',
+        description_html = '',
+        visibillity = 'draft',
+    )
+    db.add(work_orm)
+    db.commit()
+    db.refresh(work_orm)
+    return work_orm.id
+
+def set_work(db: Session, title: str, description: str, user_id: str, community_id: str, visibillity: str, thumbnail_asset_id: str, assets_id: List[str], urls: List[BaseUrlInfo]) -> Work:
     if title == '':
         raise HTTPException(status_code=400, detail="Title is empty")
 
     md = markdown.Markdown(extensions=['tables'])
     work_orm = models.Work(
-        title=title,
-        description=description,
-        description_html=md.convert(description),
-        user_id=user_id,
-        community_id=community_id,
-        work_url=work_url,
-        github_url=github_url,
-        private=private
+        title = title,
+        description = description,
+        description_html = md.convert(description),
+        user_id = user_id,
+        community_id = community_id,
+        visibillity = visibillity,
     )
     db.add(work_orm)
     db.commit()
     db.refresh(work_orm)
 
-    print(work_orm.user_id)
-    print(work_orm.community_id)
+    for asset_id in assets_id:
+        asset_orm = db.query(models.Asset).get(asset_id)
+        if asset_orm is None:
+            raise HTTPException(status_code=400, detail='This asset id is invalid.')
+        asset_orm.work_id = work_orm.id
+
+    for url in urls:
+        create_url_info(db, url.get('url'), url.get('type', 'other'), work_orm.id, user_id)
+
+    thumbnail = db.query(models.Asset).get(thumbnail_asset_id)
+    if thumbnail is None:
+        raise HTTPException(status_code=400, detail='This thumbnail asset id is invalid.')
+    thumbnail_orm = models.Thumbnail(
+        work_id = work_orm.id,
+        asset_id = thumbnail.id
+    )
+
+    db.add(thumbnail_orm)
+
+    db.commit()
+    db.refresh(work_orm)
 
     work = Work.from_orm(work_orm)
 
@@ -37,22 +68,24 @@ def get_work_by_id(db: Session, work_id: str) -> Work:
         return None
     return Work.from_orm(work_orm)
 
-def get_works_by_limit(db: Session, limit: int, oldest_id: str, private: bool = False) -> List[Work]:
-    works_orm = db.query(models.Work).order_by(models.Work.created_at)
+def get_works_by_limit(db: Session, limit: int, oldest_id: str, auth: bool = False) -> List[Work]:
+    works_orm = db.query(models.Work).order_by(models.Work.created_at).filter(models.Work.visibillity != models.Visibillity.draft)
     if oldest_id:
         limit_work = db.query(models.Work).filter(models.Work.id == oldest_id).first()
         limit_created_at = limit_work.created_at
         works_orm = works_orm.filter(models.Work.created_at > limit_created_at)
-    if not private:
-        works_orm = works_orm.filter(models.Work.private == False)
+    if not auth:
+        works_orm = works_orm.filter(models.Work.visibillity == models.Visibillity.public)
     works_orm = works_orm.limit(limit)
     works_orm = works_orm.all()
     works = list(map(Work.from_orm, works_orm))
     return works
 
-def get_work_by_id(db: Session, work_id: str, private: bool = False) -> Work:
+def get_work_by_id(db: Session, work_id: str, auth: bool = False) -> Work:
     work_orm = db.query(models.Work).get(work_id)
+    if work_orm.visibillity == models.Visibillity.draft:
+        raise HTTPException(status_code=400, detail="This work is a draft work.")
     work = Work.from_orm(work_orm)
-    if work.private and not private:
+    if work.visibillity == models.Visibillity.private and not auth:
         raise HTTPException(status_code=403, detail="This work is a private work. You need to sign in.")
     return work
--- routers/assets/__init__.py
+++ routers/assets/__init__.py
@@ -1,3 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from fastapi.datastructures import UploadFile
+from fastapi.param_functions import Body, Depends, File
+from sqlalchemy.orm.session import Session
+from cruds.assets import create_asset
+from cruds.users.auth import GetCurrentUser
+from db import get_db
+from schemas.asset import Asset, BaseAsset
+from schemas.user import User
 
 asset_router = APIRouter()
+
+@asset_router.post('', response_model=Asset)
+async def post_asset(payload: BaseAsset = Body(...), file: UploadFile = File(''), db: Session = Depends(get_db), user: User = Depends(GetCurrentUser())):
+    created_asset = None
+    if file is None:
+        raise HTTPException(status_code=400, detail='UploadFile is not found')
+    created_asset = create_asset(db, user.id, payload.asset_type, file)
+    return created_asset
