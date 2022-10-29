0f578da7bbae5c927ef315ac3dce118b6d8a27ac
PigeonsHouse <bullbull1230@gmail.com>
2022-10-30 03:13:33 +0900

    [update] 型の明言

--- routers/assets/__init__.py
+++ routers/assets/__init__.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm.session import Session
 from cruds.assets import create_asset, delete_asset_by_id
 from cruds.users.auth import GetCurrentUser
 from db import get_db
+from db.models import AssetType
 from schemas.asset import Asset
 from schemas.user import User
 from schemas.common import DeleteStatus
@@ -13,7 +14,7 @@ from schemas.common import DeleteStatus
 asset_router = APIRouter()
 
 @asset_router.post('', response_model=Asset)
-async def post_asset(file: UploadFile = File(...), asset_type: str = Form(...), db: Session = Depends(get_db), user: User = Depends(GetCurrentUser())):
+async def post_asset(file: UploadFile = File(...), asset_type: AssetType = Form(...), db: Session = Depends(get_db), user: User = Depends(GetCurrentUser())):
     if file is None:
         raise HTTPException(status_code=400, detail='UploadFile is not found')
     created_asset = create_asset(db, user.id, asset_type, file)
