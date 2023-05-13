7ee5143b4ef0a4500ed2e7c4f9fd5388824d3785
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-05-14 02:58:26 +0900

    [change] cros originの設定記述追加

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routers import router
-from db import engine
-from db.models import Base
-from fastapi.staticfiles import StaticFiles
-import os
 from utils.limit_upload_size import LimitUploadSize
+
+import os
 
 app = FastAPI(title="toybox-server")
 
 app.add_middleware(LimitUploadSize, max_upload_size=2_147_483_648)
 
-origins = ["http://localhost:8080"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=os.environ.get("ALLOW_ORIGIN_URLS").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
