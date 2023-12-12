0e0e578cc1f58395e3b391d1920a0dd73d032232
claustra01 <shinya430.g@gmail.com>
2023-12-13 03:12:12 +0900

    add: ブログ更新APIの追加

--- blogs/cruds/blogs/blogs.py
+++ blogs/cruds/blogs/blogs.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from fastapi import HTTPException
 from sqlalchemy import desc
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 
 from blogs.db import models as blog_models
@@ -203,3 +204,127 @@ def delete_blog_by_id(db: Session, blog_id: str, user_id: str) -> DeleteStatus:
     db.delete(blog_orm)
     db.commit()
     return {"status": "OK"}
+
+
+def replace_blog(
+    db: Session,
+    blog_id: str,
+    user_id: str,
+    title: str,
+    body_text: str,
+    visibility: Visibility,
+    thumbnail_asset_id: str,
+    assets_id: list[str],
+    tags_id: list[str],
+    published_at: Optional[datetime],
+) -> Blog:
+    blog_orm = db.query(blog_models.Blog).get(blog_id)
+    if blog_orm is None:
+        raise HTTPException(status_code=404, detail="blog is not found")
+    if blog_orm.user_id != user_id:
+        raise HTTPException(status_code=403, detail="this work's author isn't you")
+    if title == "":
+        raise HTTPException(status_code=400, detail="title is empty")
+    if thumbnail_asset_id == "":
+        raise HTTPException(status_code=400, detail="thumbnail is empty")
+
+    if db.transaction is None:
+        db.begin()
+
+    try:
+        # DB更新
+        blog_orm.title = title
+        blog_orm.body_text = body_text
+        blog_orm.visibility = visibility
+        blog_orm.published_at = published_at
+
+        # 使用していないassetsの紐付けを削除
+        old_assets_orm = (
+            db.query(blog_models.BlogAsset).filter_by(blog_id=blog_id).all()
+        )
+        for old_asset_orm in old_assets_orm:
+            if old_asset_orm.id not in assets_id:
+                old_asset_orm.blog_id = None
+
+        # 新しいassetsの紐付け
+        new_assets_id = set(assets_id) - set(
+            [old_asset_orm.id for old_asset_orm in old_assets_orm]
+        )
+        new_assets_orm = (
+            db.query(blog_models.BlogAsset)
+            .filter(blog_models.BlogAsset.id.in_(new_assets_id))
+            .all()
+        )
+        for new_asset_orm in new_assets_orm:
+            new_asset_orm.blog_id = blog_id
+        existing_assets_orm = (
+            db.query(blog_models.BlogAsset).filter_by(blog_id=blog_id).all()
+        )
+        existing_assets_id = set(
+            [existing_asset_orm.id for existing_asset_orm in existing_assets_orm]
+        )
+        for asset_id in assets_id:
+            if asset_id not in existing_assets_id:
+                raise HTTPException(
+                    status_code=400, detail=f'asset_id "{asset_id}" is invalid'
+                )
+
+        # 使用していないtagsの紐付け中間テーブルを削除
+        old_tags_orm = (
+            db.query(blog_models.BlogTagging).filter_by(blog_id=blog_id).all()
+        )
+        for old_tag_orm in old_tags_orm:
+            if old_tag_orm.tag_id not in tags_id:
+                db.delete(old_tag_orm)
+
+        # 新しいtagsを中間テーブルで紐付け
+        new_tags_id = set(tags_id) - set(
+            [old_tag_orm.tag_id for old_tag_orm in old_tags_orm]
+        )
+        for new_tag_id in new_tags_id:
+            new_tagging_orm = blog_models.BlogTagging(
+                blog_id=blog_id, tag_id=new_tag_id
+            )
+            db.add(new_tagging_orm)
+        existing_tags_orm = (
+            db.query(blog_models.BlogTagging).filter_by(blog_id=blog_id).all()
+        )
+        existing_tags_id = set(
+            [existing_tag_orm.tag_id for existing_tag_orm in existing_tags_orm]
+        )
+        for tag_id in tags_id:
+            if tag_id not in existing_tags_id:
+                raise HTTPException(
+                    status_code=400, detail=f'tag_id "{tag_id}" is invalid'
+                )
+
+        # Thumbnailを中間テーブルで紐付け
+        thumbnail = db.query(blog_models.BlogAsset).get(thumbnail_asset_id)
+        if thumbnail is None:
+            raise HTTPException(
+                status_code=400,
+                detail=f'thumbnail_asset_id "{thumbnail_asset_id}" is invalid',
+            )
+        thumbnail_orm = (
+            db.query(blog_models.BlogThumbnail).filter_by(blog_id=blog_id).first()
+        )
+        if thumbnail_orm.asset_id != thumbnail.id:
+            new_thumbnail_orm = blog_models.BlogThumbnail(
+                blog_id=blog_id, asset_id=thumbnail.id
+            )
+            db.add(new_thumbnail_orm)
+
+        # commit
+        db.commit()
+
+    except SQLAlchemyError as e:
+        # rollback
+        db.rollback()
+        raise HTTPException(
+            status_code=500, detail=f"database transaction error: {str(e)}"
+        )
+
+    finally:
+        blog = Blog.from_orm(blog_orm)
+
+    return blog
