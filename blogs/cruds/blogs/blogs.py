e146fb19ae548d21f2826dee7f09e2ad83878f2f
Bokume2 <nagatomoyuu2@gmail.com>
2025-04-03 23:14:04 +0900

    fix: ブログのサムネイルが更新できない問題を修正

--- blogs/cruds/blogs/blogs.py
+++ blogs/cruds/blogs/blogs.py
@@ -315,6 +315,9 @@ def replace_blog(
             db.query(blog_models.BlogThumbnail).filter_by(blog_id=blog_id).first()
         )
         if thumbnail_orm.asset_id != thumbnail.id:
+            # 古いThumbnailを紐付け解除
+            db.delete(thumbnail_orm)
+            # 新しいThumbnailを紐付け
             new_thumbnail_orm = blog_models.BlogThumbnail(
                 blog_id=blog_id, asset_id=thumbnail.id
             )
